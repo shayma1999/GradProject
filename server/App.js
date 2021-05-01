e1efe5dc92848b44145b3c4a0cd8d69158e894d8
shayma1999 <72220127+shayma1999@users.noreply.github.com>
2021-05-01 14:11:47 +0300

    Update App.js

--- server/App.js
+++ server/App.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mysql = require('mysql');
+
+//create database connection
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'gradproject'
+});
+//connect to database
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('Mysql Connected...');
+});
+
 
 const App = express();
-
+App.use(bodyParser.json({ type: '/' }));
 // parse requests of content-type: application/json
 App.use(bodyParser.json());
 
@@ -14,6 +29,14 @@ App.get("/", (req, res) => {
   res.json({ message: "Welcome to oregano grad application." });
 });
 
+App.get(/search\/(?:([^\/]+?))\/?$/i, function (req, res) {
+  var search = req.params[0]
+  var sql = "SELECT * from finalfinal WHERE list_timeA LIKE '%"+search+"%'"
+  conn.query(sql, function (err, result) {
+    if (err) res.send(err)
+    res.send(result)
+  })
+})
 
 require("./routes/names.routes.js")(App);
 
